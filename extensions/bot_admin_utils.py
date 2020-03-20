7a680a2344cbe4ddb66f7dd5bdba762b2bcacd2f
Fwf <ffillingham@gmail.com>
2020-03-20 17:01:51 +0000

    Add cog description and graceful shutdown method

--- extensions/bot_admin_utils.py
+++ extensions/bot_admin_utils.py
@@ -1,9 +1,11 @@
 from discord.ext import commands
-
+import asyncio
 import logging
 
 
 class AdminUtilsCog(commands.Cog, name="Admin Utilities"):
+    """Cog for administrative commands, be these for users or to manage the bot
+    """
     def __init__(self, bot):
         self.bot = bot
         self.logger = logging.getLogger("SVGEBot.AdminUtils")
@@ -20,7 +22,19 @@ class AdminUtilsCog(commands.Cog, name="Admin Utilities"):
                 sender_is_admin = True
                 break
         return sender_is_admin
-                
+
+    @commands.command()
+    async def shutdown(self, ctx):
+        """Shuts the bot process down gracefully."""
+        for cog in self.bot.cogs:
+            self.bot.unload_extension(cog[0])
+            self.logger.info(f"Unloaded {cog[0]}")
+        await self.bot.logout()
+        self.logger.info("Logged out and closed Discord API connection")
+        await asyncio.sleep(5)
+        self.logger.info("Closing process")
+        exit(0)
+
 
 def setup(bot):
     bot.add_cog(AdminUtilsCog)
