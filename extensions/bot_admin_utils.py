ca6747ed464fb9337f991c2b597ebfeb98f9deb2
Fwf <ffillingham@gmail.com>
2020-03-22 16:23:57 +0000

    Add change_presence command
    Credit: JayDwee (CyclopsBot)

--- extensions/bot_admin_utils.py
+++ extensions/bot_admin_utils.py
@@ -36,7 +36,41 @@ class AdminUtilsCog(commands.Cog, name="Admin Utilities"):
         await self.bot.logout()
         self.logger.info("Logged out and closed Discord API connection")
         self.logger.info("Closing process")
+        # This sleep is to avoid background loops getting messed with by an
+        # abrupt exit.
+        await asyncio.sleep(4)
         exit(0)
+
+    @commands.command()
+    async def change_presence(self, ctx, activity, text=">>help"):
+        """Changes the bot "presence" statement to that defined in command,
+        permitting it is one of those permitted by discord.
+
+        Command originally written for CyclopsBot by JayDwee.
+
+        :arg ctx: Command context, auto-filled by API wrapper.
+        :arg activity: Activity for the bot to display, must be one of:
+
+        :arg text: Text following the activity term"""
+
+        activity_list = {
+            "watching": discord.ActivityType.watching,
+            "streaming": discord.ActivityType.streaming,
+            "playing": discord.ActivityType.playing,
+            "listening": discord.ActivityType.listening
+        }
+
+        if activity.lower() not in activity_list.keys():
+            await ctx.send(f'"{activity}" is an invalid activity. "WatchingW, "streaming", '
+                           f'"playing", and "listening" are currently supported',
+                           delete_after=self.bot.delete_msg_after)
+            return
+
+        activity_type_to_show = discord.Activity(activity=activity_list[activity.lower()],
+                                                 name=text)
+        await self.bot.change_presence(activity=activity_type_to_show)
+        self.logger.info(f"Activity changed to {activity} {text}")
+        await ctx.send(f"Activity changed as requested.", delete_after=self.bot.delete_msg_after)
 
 
 def setup(bot):
