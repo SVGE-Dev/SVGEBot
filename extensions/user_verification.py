eb9e6469a6e5d3738a76f2b9a5498792c8734577
Fwf <ffillingham@gmail.com>
2020-03-25 04:01:23 +0000

    Complete verification flow (see below):
    - Emails now send from chosen noreply email to verification requests that meet requirements.
    - Outsourced code-guild-inference to __classmethod
    - Improved use error reporting l.356
    - Code based second part of verification flow now functions as intended, assigning the role id given in user_verification_config.json

--- extensions/user_verification.py
+++ extensions/user_verification.py
@@ -1,9 +1,16 @@
 import logging
 import re
+import os
+import json
 import datetime
 import hashlib
+import asyncio
 from typing import Union
+from shutil import copyfile
+from email.message import EmailMessage
 
+import aiosmtplib
+import discord
 from discord.ext import commands
 
 
@@ -11,12 +18,39 @@ class UserVerification(commands.Cog, name="User Verification"):
     def __init__(self, bot):
         self.bot = bot
         self.db_pool_cog = None
+        self.__cog_config = None
+        self.__get_config()
         self.logger = logging.getLogger("SVGEBot.Verification")
         self.logger.info("Loaded User Verification")
 
     @property
     def cmd_prefix(self):
         return self.bot.bot_config['cmd_prefix']
+
+    def __get_config(self, run_counter=0):
+        # This function needs to be moved into a shared extension, and likely will be soon.
+        # Currently a duplicate exists within ./extensions/db_conn.py
+        cog_conf_location = "./extensions/extension_configs/user_verification_config.json"
+        default_cog_conf_loc = "./extensions/extension_configs/user_verification_config_default" \
+                               ".json"
+        if os.path.exists(cog_conf_location):
+            with open(cog_conf_location) as cog_config_obj:
+                self.__cog_config = json.load(cog_config_obj)
+        else:
+            self.logger.warning("Main config not found, copying default config"
+                                "and attempting to use instead.")
+            copyfile(default_cog_conf_loc, cog_conf_location)
+            run_counter += 1
+            if run_counter < 2:
+                self.__get_config(run_counter=run_counter)
+            else:
+                self.logger.exception(f"Default config malconfigured or otherwise missing "
+                                      f"from expected location: {cog_conf_location}, and"
+                                      f"{default_cog_conf_loc}. Please visit: "
+                                      f"https://github.com/SVGE-Dev/SVGEBot and reacquire the "
+                                      f"config files.")
+                input()
+                exit(1)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -45,7 +79,7 @@ class UserVerification(commands.Cog, name="User Verification"):
                     ON DUPLICATE KEY UPDATE discord_username=(%(d_uname)s);
                     INSERT INTO member_verification (
                         discord_user_id, email, verification_key, last_verification_req
-                    ) VALUES (%(d_uid)s, '', '', %(datetime_old)s)
+                    ) VALUES (%(d_uid)s, NULL, '', %(datetime_old)s)
                     ON DUPLICATE KEY UPDATE discord_user_id=discord_user_id""",
                     {"d_uid": member.id, "d_uname": member.name+"#"+member.discriminator,
                      "datetime_old": datetime.datetime(2000, 1, 1)}
@@ -88,7 +122,7 @@ class UserVerification(commands.Cog, name="User Verification"):
                     except KeyError:
                         guild_alias = member.guild.id
                     await member.send(
-                        f"You have been added to {member.guild.name}'s verification system."
+                        f"You have been added to {member.guild.name}'s verification system. "
                         f"In order to verify, you will need to provide a University of "
                         f"Southampton (@soton.ac.uk) email address in this DM.\n\n"
                         f"The command will look something like:\n"
@@ -116,6 +150,7 @@ class UserVerification(commands.Cog, name="User Verification"):
 
     @commands.group()
     @commands.dm_only()
+    @commands.max_concurrency(1, per=commands.BucketType.user)
     async def verify(self, ctx):
         """Command group for user verification, does nothing when invoked
         directly."""
@@ -123,12 +158,148 @@ class UserVerification(commands.Cog, name="User Verification"):
             await ctx.send(f"You need to use a subcommand with this command group.\n\n"
                            f"Use `{self.cmd_prefix}help verify to see child commands.")
 
-    @verify.command()
-    async def code(self, ctx, verification_code):
-        pass
+    async def __gen_guild_id_from_verification_code(self, ctx, verification_code):
+        guild_alias = verification_code.rsplit("-", 1)[-1]
+        try:
+            guild_id = str(self.db_pool_cog.cog_config["guild_aliases"][guild_alias.lower()])
+        except KeyError:
+            self.logger.debug(f"Failed to generate guild_id from alias: {verification_code},"
+                              f" {guild_alias}.")
+            await ctx.send("The verification code you submitted was invalid, ensure you "
+                           "include hyphens in the code.")
+            return None
+        return guild_id
+
+    async def __member_verify_update(self, target_user_id, guild_id):
+        guild_object = self.bot.get_guild(int(guild_id))
+        member_object = guild_object.get_member(target_user_id)
+        verify_role_object = guild_object.get_role(self.__cog_config["discord"]["verified_role_id"])
+
+        try:
+            await member_object.add_roles(verify_role_object)
+            return True
+        except discord.HTTPException as add_role_error:
+            self.logger.exception(f"Role addition failed during automated verification. Error: "
+                                  f"\n\n{add_role_error}")
+            return False
 
     @verify.command()
-    @commands.max_concurrency(1, per=commands.BucketType.user)
+    async def code(self, ctx, verification_code):
+        guild_id = await self.__gen_guild_id_from_verification_code(ctx, verification_code)
+        async with self.db_pool_cog.conn_pool.acquire() as connection:
+            async with connection.cursor() as cursor:
+                guild_db_name = "guild_"+str(guild_id)
+                await cursor.execute("""USE `%s`""", guild_db_name)
+                await cursor.execute(
+                    """SELECT *
+                    FROM guild_members
+                    WHERE discord_user_id = %(d_uid)s""",
+                    {"d_uid": ctx.author.id})
+                guild_member_result = list(await cursor.fetchone())
+                await cursor.execute(
+                    """SELECT *
+                    FROM member_verification
+                    WHERE discord_user_id = %(d_uid)s""",
+                    {"d_uid": ctx.author.id}
+                )
+                verification_query_result = list(await cursor.fetchone())
+                self.logger.debug(f"User records found: \n{guild_member_result}"
+                                  f"\n{verification_query_result}")
+                if guild_member_result[3] == 1:
+                    self.logger.debug(f"Code verification for {ctx.author} failed due "
+                                      f"to user already validated.")
+                    await ctx.send("You have already been verified, if you think "
+                                   "this was in error, please contact an administrator.")
+                    return
+                if verification_code != verification_query_result[2]:
+                    self.logger.debug(f"Code verification for {ctx.author} failed due "
+                                      f"to improper verification code.")
+                    await ctx.send("The verification code you have provided is invalid, "
+                                   "please ensure you use an exact copy from the "
+                                   "email you were sent. ")
+                    return
+
+                # At this point we know we have a valid verification request, let's
+                # update the user's record and null their verification key.
+                await cursor.execute(
+                    """UPDATE guild_members 
+                    SET verified = 1
+                    WHERE discord_user_id = %(d_uid)s;
+                    UPDATE member_verification
+                    SET verification_key = NULL
+                    WHERE discord_user_id = %(d_uid)s""",
+                    {"d_uid": ctx.author.id}
+                )
+
+                verification_success = await self.__member_verify_update(ctx.author.id, guild_id)
+                if not verification_success:
+                    await ctx.send(f"Verification role allocation failed. Please contact an "
+                                   f"administrator with the datetime of this exception: "
+                                   f"{datetime.datetime.now()}")
+                else:
+                    await ctx.send(f'Verification success, welcome to '
+                                   f'{verification_code.rsplit("-", 1)[-1]}')
+
+    async def send_verification_code_email(self, ctx, verification_code, curr_try=0):
+        guild_id = await self.__gen_guild_id_from_verification_code(ctx, verification_code)
+        guild_alias = verification_code.rsplit("-", 1)[-1]
+        guild_db_name = "guild_" + str(guild_id)
+
+        async with self.db_pool_cog.conn_pool.acquire() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute("""USE `%s`""", (guild_db_name,))
+                await cursor.execute(
+                    """SELECT email
+                    FROM member_verification
+                    WHERE discord_user_id=%s""",
+                    (ctx.author.id,)
+                )
+                user_email_address = await cursor.fetchone()
+
+        if user_email_address is None and curr_try < 5:
+            curr_try += 1
+            self.logger.debug("Retrying email address fetching")
+            await asyncio.sleep(curr_try)
+            return await self.send_verification_code_email(ctx, verification_code,
+                                                           curr_try=curr_try)
+        formatted_bot_email_addr = f"{self.__cog_config['account']['username']}@gmail.com"
+        formatted_bot_email_subj = f"No Reply | {guild_alias.upper()} Automated Verification Email"
+        email = EmailMessage()
+        email["From"] = formatted_bot_email_addr
+        email["To"] = user_email_address
+        email["Subject"] = formatted_bot_email_subj
+        email.set_content(
+            f"Hi {ctx.author},\n\nThis is an automated verification "
+            f"email for {guild_alias.upper()}, if you did not request "
+            f"verification, please notify a guild administrator.\n\n"
+            f"In order to use your verification code, send the following "
+            f"command in direct messages to {self.bot.user}:\n"
+            f"{self.cmd_prefix}verify code {verification_code}\n\n"
+            f"Please contact an administrator if you have trouble with "
+            f"this command.\n\n"
+            f"This email address is not monitored."
+        )
+
+        try:
+            async with aiosmtplib.SMTP(**self.__cog_config["email"], **self.__cog_config[
+                    "account"]) as smtp_client:
+                response = await smtp_client.send_message(email)
+        except aiosmtplib.SMTPException as smtp_error:
+            self.logger.exception(f"Unable to complete email operation:\n\n{smtp_error}")
+            await ctx.send(f"There was an exception while sending your verification email, "
+                           f"please contact an administrator with the date and time of this "
+                           f"error: `{datetime.datetime.now()}`")
+            return
+
+        self.logger.debug(f"Sent email to {user_email_address}, response: {response}.")
+        await ctx.send(f"A verification email has been sent to you from: "
+                       f"{formatted_bot_email_addr}, with the subject: "
+                       f"{formatted_bot_email_subj}. \n\n"
+                       f"Please make sure to check your spam and filtered mailboxes, "
+                       f"if you have not received an email after a couple of minutes, "
+                       f"check whether the email address you have entered is correct.")
+
+    @verify.command()
     async def email(self, ctx, email_address, pre_verification_id):
         """Allows users to begin the verification process in a given discord guild.
 
@@ -182,7 +353,7 @@ class UserVerification(commands.Cog, name="User Verification"):
 
                 if user_result[3] == 1:
                     await ctx.send("You have already been verified, if you think "
-                                   "this is a mistake, please contact an administrator.")
+                                   "this was in error, please contact an administrator.")
                     return
 
                 await cursor.execute(
@@ -215,6 +386,8 @@ class UserVerification(commands.Cog, name="User Verification"):
                         ) VALUES (%s, %s, %s, %s)""",
                     tuple(user_verification_result)
                 )
+
+                await self.send_verification_code_email(ctx, verification_code)
 
 
 def setup(bot):
